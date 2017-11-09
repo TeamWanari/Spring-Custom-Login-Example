913b7ccbf76362ecb1bc21b6907bdbd417544d0c
Sükein Alex <suxy@wanari.com>
2017-11-09 19:29:05 +0100

    Initial commit

---
+++ src/main/java/com/wanari/customlogin/example/CustomLoginExampleApplication.java
@@ -0,0 +1,12 @@
+package com.wanari.customlogin.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLoginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomLoginExampleApplication.class, args);
+	}
+}
